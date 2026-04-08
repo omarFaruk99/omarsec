25fa7dd3d425bc773e53efbbac44f1818e8d564c
Omar Faruk <90040957+omarFaruk99@users.noreply.github.com>
2026-04-08 11:59:23 +0600

    Update navbar logo text from 'OmarSec' to '###'

--- app/layout.jsx
+++ app/layout.jsx
@@ -32,7 +32,7 @@ export const metadata = {
 
 const navbar = (
   <Navbar
-    logo={<span style={{ fontWeight: 800, fontSize: '1.1rem' }}>OmarSec</span>}
+    logo={<span style={{ fontWeight: 800, fontSize: '1.1rem' }}>###</span>}
     projectLink="https://github.com/omarFaruk99/omarsec"
   />
 )
